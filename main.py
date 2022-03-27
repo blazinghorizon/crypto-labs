1b6d68d66ac7e70952e2c60dc5a9f7faf690bb88
Nikita Semenov <60959784+blazinghorizon@users.noreply.github.com>
2022-03-28 00:42:14 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,94 @@
+# 1.2 lab
+
+from turtle import xcor
+import numpy
+import utils
+
+def generate_p_q():
+    p = 0
+    q = 0
+    n = 16
+
+    while True:
+        p = utils.getLowLevelPrime(n)
+        if not utils.isMillerRabinPassed(p):
+            continue
+        else:
+           # print(n, "bit prime is: \n", p)
+            break
+
+    while True:
+        q = utils.getLowLevelPrime(n)
+        if not utils.isMillerRabinPassed(q):
+            continue
+        else:
+         #   print(n, "bit prime is: \n", q)
+            break
+
+    return p, q
+
+#print(utils.power(2, 20))
+#print(utils.modInverse(3, 26))
+#print(utils.modInverse2(3, 26))
+
+e = int(input('input e'))
+
+while True:
+    p, q = generate_p_q()
+    phi = (p-1)*(q-1)
+
+    if utils.gcd(e, phi) == 1:
+        break
+    else:
+        e += 1
+
+print(utils.power3(105, 250))
+
+_n = p*q
+d = utils.modInverse2(e, phi)
+print(f'E = {e}, p = {p}, q = {q}, phi = {phi}, n = {_n}, d = {d}\n')
+
+print(f'open key - ({e}, {_n}), closed key - ({d}, {_n})')
+
+msg = 20
+
+print("Message data = ", msg)
+
+c = utils.power(msg, e) % _n
+
+print("\nEncrypted data = ", c)
+
+m = utils.power3(c, d) % _n
+
+print("\nOriginal Message Sent = ", m)
+
+
+'''
+while True:
+        n = 1024
+        p = utils.getLowLevelPrime(n)
+        print('p')
+        q = utils.getLowLevelPrime(n)
+        print(utils.isMillerRabinPassed(p))
+        print(utils.isMillerRabinPassed(q))
+        break
+       # if not utils.isMillerRabinPassed(p):
+          #  continue
+      #  if not utils.isMillerRabinPassed(q):
+           # continue
+        else:
+            print('xx')
+            phi = (p-1)*(q-1)
+
+            if utils.gcd(e, phi) == 1:
+                print(f"{n} bit prime 1st is: {p}\n prime 2nd is: {q}")
+                break
+            else:
+                continue
+
+_n = p*q
+#d = utils.modInverse2(e, phi)
+#print(f'E = {e}, p = {p}, q = {q}, phi = {phi}, n = {_n}, d = {d}\n')
+
+
+'''
---
+++ utils.py
@@ -0,0 +1,140 @@
+import random
+
+# sqr func
+def power(num1, num2):
+    if num2 == 0:
+        return 1
+    elif num2 == 1:
+        return num1
+    elif num2 % 2 != 0:
+        return num1 * power(num1, num2 - 1)
+    elif num2 % 2 == 0:
+        return power(num1 * num1, num2 / 2)
+
+def power2(base, exp):
+    """ Fast power calculation using repeated squaring """
+    if exp < 0:
+        return 1 / power(base, -exp)
+    ans = 1
+    while exp:
+        if exp & 1:
+            ans *= base
+        exp >>= 1
+        base *= base
+    return ans
+
+def power3(a, b):
+    x = 0
+
+    if (b == 0):
+        return 1
+
+    if b % 2 == 0 :
+        x = power3(a, b/2)
+        return x * x
+
+    x = power3(a, (b - 1)/2)
+    x = x * x
+    return a * x
+
+# gcd func
+def gcd(num1, num2):
+    while num1 != 0 and num2 != 0:
+        if num1 >= num2:
+            num1 %= num2
+        else:
+            num2 %= num1
+    return num1 or num2
+
+# naive method
+def modInverse(a, m):
+     
+    for x in range(1, m):
+        if (((a % m) * (x % m)) % m == 1):
+            return x
+    return -1
+
+# extended euclid algorithm
+def modInverse2(a, m):
+    m0 = m
+    y = 0
+    x = 1
+ 
+    if (m == 1):
+        return 0
+ 
+    while (a > 1):
+ 
+        # q is quotient
+        q = a // m
+ 
+        t = m
+ 
+        # m is remainder now, process
+        # same as Euclid's algo
+        m = a % m
+        a = t
+        t = y
+ 
+        # Update x and y
+        y = x - q * y
+        x = t
+ 
+    # Make x positive
+    if (x < 0):
+        x = x + m0
+ 
+    return x
+ 
+# Pre generated primes
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349]
+ 
+def nBitRandom(n):
+    return random.randrange(2**(n-1)+1, 2**n - 1)
+ 
+def getLowLevelPrime(n):
+    '''Generate a prime candidate divisible
+    by first primes'''
+    while True:
+        # Obtain a random number
+        pc = nBitRandom(n)
+ 
+         # Test divisibility by pre-generated
+         # primes
+        for divisor in first_primes_list:
+            if pc % divisor == 0 and divisor**2 <= pc:
+                break
+        else: return pc
+ 
+def isMillerRabinPassed(mrc):
+    '''Run 20 iterations of Rabin Miller Primality test'''
+    maxDivisionsByTwo = 0
+    ec = mrc-1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert(2**maxDivisionsByTwo * ec == mrc-1)
+ 
+    def trialComposite(round_tester):
+        if pow(round_tester, ec, mrc) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2**i * ec, mrc) == mrc-1:
+                return False
+        return True
+ 
+    # Set number of trials here
+    numberOfRabinTrials = 20
+    for i in range(numberOfRabinTrials):
+        round_tester = random.randrange(2, mrc)
+        if trialComposite(round_tester):
+            return False
+    return True
